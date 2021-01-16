e4e2abfd53bca5f7822c28140c4338d2a9a7c873
Sankalan Paul Chowdhury <53177253+sankalan4198@users.noreply.github.com>
2021-01-16 10:15:07 +0530

    Create LargestAmongThree.java

---
+++ LargestAmongThree.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class LargestAmongThree {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner((System.in));
+        System.out.println("Enter three number: ");
+        int a= sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        if(a>=b && a>=c)
+            System.out.println(a+" is the largest number.");
+        else if(b>=a && b>=c)
+            System.out.println(b+" is the largest number.");
+        else if(c>=a && c>=b)
+            System.out.println(c+" is the largest number.");
+    }
+}
