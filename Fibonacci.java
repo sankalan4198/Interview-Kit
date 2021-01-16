89e13150075b4dbd26d405e377773b28dec05fa3
Sankalan Paul Chowdhury <53177253+sankalan4198@users.noreply.github.com>
2021-01-16 10:11:00 +0530

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Fibonacci {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,fact=1;
+        System.out.println("Enter the value of n: ");
+        n=sc.nextInt();
+        if(n==0)
+        {
+            System.out.println("Factorial is :1");
+            return;
+        }
+        else {
+            for (i = 1; i <= n; i++) {
+                fact = fact * i;
+            }
+        }
+        System.out.println("Factorial is :"+fact);
+
+    }
+}
