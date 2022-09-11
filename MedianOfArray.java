e5fada80e30528f19d355286e77a9e177f47efea
Sankalan Paul Chowdhury <53177253+sankalan4198@users.noreply.github.com>
2022-09-11 13:14:11 +0530

    Update MedianOfArray.java

--- MedianOfArray.java
+++ MedianOfArray.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 public class MedianOfArray {
     public static void main(String args[])
     {
+        
+        /*
+        If an array is sorted, median is the middle element of an array in case of odd number of elements in an array and 
+        when number of elements in an array is even than it will be an average of two middle elements.
+        If the array is not sorted first task is to sort the array and then only the given logic can be applied */
+        
+        
+       
         double m=0;
         Scanner sc= new Scanner(System.in);
         System.out.println("Enter a number: ");
