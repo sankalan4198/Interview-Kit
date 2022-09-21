cb066d1244710c48a9c0339fcd4ab7d355de6f19
Sankalan Paul Chowdhury <53177253+sankalan4198@users.noreply.github.com>
2022-09-21 06:00:42 +0530

    Create Check If a String contains a Substring.java

---
+++ Check If a String contains a Substring.java
@@ -0,0 +1,18 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    Scanner sc=new Scanner(System.in);
+	    String str="Sankalan Paul Chowdhury";
+	    String substring="Roy";
+	    
+	    if(str.contains(substring))
+	    {
+	        System.out.println("yes");
+	    }
+	    else
+	    {
+	        System.out.println("No");
+	    }
+	}
+}
