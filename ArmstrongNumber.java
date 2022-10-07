17b1d03b6ba305ca81adcbba47b7bf0f4661c9eb
Sankalan Paul Chowdhury <53177253+sankalan4198@users.noreply.github.com>
2022-10-08 00:35:06 +0530

    Update ArmstrongNumber.java

--- ArmstrongNumber.java
+++ ArmstrongNumber.java
@@ -26,3 +26,49 @@ public class ArmstrongNumber {
         }
     }
 }
+
+
+//Method covering all number of digits
+import java.util.Scanner;
+import java.lang.Math;
+public class Main
+{
+	public static void main(String[] args) {
+	    int n,rem,c,temp,sum=0;
+	    int digitCount=0;
+		System.out.println("Enter a number: ");
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		
+		temp=n;
+		
+		//Calculating digit digitCount
+		
+		while(n!=0)
+		{
+		    n=n/10;
+		    digitCount++;
+		}
+		n=temp;
+		while(n>0)
+		{
+		    rem=n%10;
+		    c=(int)Math.pow(rem,digitCount);
+		    sum=sum+c;
+		    n=n/10;
+		    
+		}
+		//System.out.println(digitCount);
+		if(temp==sum)
+		{
+		    System.out.println("ARMSTRONGGG");
+		    
+		}
+		else
+		{
+		    System.out.println("NOT ARMSTRONGGG");
+		}
+		
+		
+	}
+}
