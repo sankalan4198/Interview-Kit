3428b1adc026315e313ae49cbf0ab9f4e8334088
Sankalan Paul Chowdhury <53177253+sankalan4198@users.noreply.github.com>
2024-07-25 20:59:02 +0530

    Create LeftRotateArray_by_OnePlace.java

---
+++ LeftRotateArray_by_OnePlace.java
@@ -0,0 +1,19 @@
+public class LeftRotateArray_by_OnePlace {
+
+    public static void main(String args[])
+    {
+        int arr[]={1,2,3,4,5};
+
+        int temp=arr[0];
+        for(int i=1;i<arr.length;i++)
+        {
+            arr[i-1]=arr[i];
+        }
+        arr[arr.length-1]=temp;
+
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
