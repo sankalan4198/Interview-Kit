7be4382e61bdb29a5792220a024b14c265b5e250
Sankalan Paul Chowdhury <53177253+sankalan4198@users.noreply.github.com>
2025-02-16 08:53:37 +0530

    Update DuplicateElementsInArray.java

--- DuplicateElementsInArray.java
+++ DuplicateElementsInArray.java
@@ -62,3 +62,31 @@ public class DuplicateElements {
     }
         
 }
+------------------------------------------------------------------------------------------
+    public class Main
+{
+    public static void main(String[] args) {
+        int arr[]={2,3,1,4,2,2,3,6,5,4};
+        int count;
+        
+        for(int i=0;i<arr.length;i++)
+        {
+            count=1;
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[i]==arr[j])
+                {
+                    count++;
+                    arr[j]='0';
+                }
+            }
+            if(count>1 && arr[i]!='0')
+            {
+                System.out.print(arr[i]+" ");
+            }
+        }
+        
+    }
+        
+}
+
