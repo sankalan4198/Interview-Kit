48ba3e51fcc0e9f3e32522eeb5a5e4d92fc3a4ae
Sankalan Paul Chowdhury <53177253+sankalan4198@users.noreply.github.com>
2025-02-16 08:38:45 +0530

    Update DuplicateElementsInArray.java

--- DuplicateElementsInArray.java
+++ DuplicateElementsInArray.java
@@ -26,3 +26,33 @@ public class DuplicateElements {
 
     }
 }
+---------------------------------------------------------------
+    //Code to print the repeated character only once.
+    //Here we add an extra check to see if the number is already printed or not
+
+    public class Main
+{
+    public static void main(String[] args) {
+        int arr[]={2,3,1,4,2,2,3,6,5,4};
+        
+        for(int i=0;i<arr.length;i++) {
+            boolean isPrinted = false;
+            for(int k=0;k<i;k++) {
+                if(arr[i] == arr[k]) {
+                    isPrinted = true;
+                    break;
+                }
+            }
+            if (isPrinted) {
+                continue;
+            }
+            
+            for(int j=i+1;j<arr.length;j++) {
+                if(arr[i] == arr[j]) {
+                    System.out.print(arr[i] + " ");
+                    break;
+                }
+            }
+        }
+    }
+}
