89a7893ffaae6285f7f982e64a0c0da6fbeb2fe8
Sankalan Paul Chowdhury <53177253+sankalan4198@users.noreply.github.com>
2025-04-23 16:11:29 +0530

    Update LongestConsecutiveSequenceInArray.java

--- LongestConsecutiveSequenceInArray.java
+++ LongestConsecutiveSequenceInArray.java
@@ -39,3 +39,29 @@ public class Main
 	}
 	
 }
+
+-------------------------------------------
+
+	// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.Arrays;
+class Main {
+    public static void main(String[] args) {
+        int arr[]={3,8,5,7,6};
+        Arrays.sort(arr);
+        int cnt=1;
+        for(int i=1;i<arr.length;i++)
+        {
+            if(arr[i]-arr[i-1]==1)
+            {
+                System.out.print(arr[i-1]+" ");
+                while(i<arr.length && arr[i]-arr[i-1]==1)
+                {
+                    System.out.print(arr[i]+" ");
+                    i++;
+                }
+            }
+        }
+        
+    }
+}
