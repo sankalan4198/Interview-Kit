cf9f882fed2180bb5bda90a9011d776fcea73335
Sankalan Paul Chowdhury <53177253+sankalan4198@users.noreply.github.com>
2026-03-03 19:27:12 +0530

    Update MoveAllNegativeElementsTo End.java

--- MoveAllNegativeElementsTo End.java
+++ MoveAllNegativeElementsTo End.java
@@ -26,3 +26,41 @@ public class Main
 		 }
 	}
 }
+_____________________________________
+
+	without extra space- Two pointers
+
+	public class Main
+{
+	public static void main(String[] args) {
+		int arr[]={1,2,-1,-4,5,-3,6};
+		int i=0,j=arr.length-1;
+		//Move 
+		
+		while(i<j)
+		{
+		    if(arr[i]<0 && arr[j]>0)
+		    {
+		        int temp=arr[i];
+		        arr[i]=arr[j];
+		        arr[j]=temp;
+		        i++;
+		        j--;
+		    }
+		    else if((arr[i]>0 && arr[j]<0)|| (arr[i]>0 && arr[j]>0))
+		    {
+		        i++;
+		    }
+		    else
+		    {
+		        j--;
+		    }
+		    
+		}
+		
+		for(i=0;i<arr.length;i++)
+		{
+		    System.out.print(arr[i]+" ");
+		}
+	}
+}
