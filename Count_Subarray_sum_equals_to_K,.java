c4196ea8a90268d2f31049d8372cac62bb49d96a
Sankalan Paul Chowdhury <53177253+sankalan4198@users.noreply.github.com>
2026-05-28 20:50:36 +0530

    Update Count_Subarray_sum_equals_to_K,.java

--- Count_Subarray_sum_equals_to_K,.java
+++ Count_Subarray_sum_equals_to_K,.java
@@ -29,3 +29,43 @@ public class Main
 		System.out.println(cnt);
 	}
 }
+
+-----------------------------------
+
+//IF I WANT TO PRINT THE SUBARRAYS
+
+	public class Main {
+
+    public static void main(String[] args) {
+
+        int arr[] = {3,1,2,4};
+
+        int k = 6;
+
+        int cnt = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+
+            int sum = 0;
+
+            for(int j = i; j < arr.length; j++) {
+
+                sum = sum + arr[j];
+
+                if(sum == k) {
+
+                    cnt++;
+
+                    // print subarray
+                    for(int x = i; x <= j; x++) {
+                        System.out.print(arr[x] + " ");
+                    }
+
+                    System.out.println();
+                }
+            }
+        }
+
+        System.out.println("Count = " + cnt);
+    }
+}
